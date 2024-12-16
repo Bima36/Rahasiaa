62b1697f6bdb07a64752764b008d3c3ba66850ea
Bima36 <153841666+Bima36@users.noreply.github.com>
2024-12-17 01:56:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function openEnvelope() {
-  document.getElementById('envelope').style.display = 'none';
-  document.getElementById('main-content').classList.remove('hidden');
-}
-
 function handleChoice(choice) {
   if (choice === 'yes') {
     window.location.href = 'hadiah.html?response=yes';
